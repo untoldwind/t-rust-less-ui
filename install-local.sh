01c0e36ddc799b14d3b024a9fa0f6d03f3406634
Bodo Junglas <junglas@objectcode.de>
2021-08-19 14:52:02 +0200

    Fix local install script

--- install-local.sh
+++ install-local.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+yarn clean
 yarn dist
-cp dist/t-rust-less-ui.AppImage "$HOME/bin/t-rust-less-ui"
+cp src-tauri/target/release/t-rust-less-ui "$HOME/bin/t-rust-less-ui"
 
 cat <<END >"$HOME/.local/share/applications/t-rust-lest-ui.desktop"
 [Desktop Entry]
