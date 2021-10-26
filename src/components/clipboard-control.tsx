da0cbc158ee91d21cf243f5e9b779b3d8f309f12
Bodo Junglas <junglas@objectcode.de>
2021-10-26 15:28:13 +0200

    Improve optics

--- src/components/clipboard-control.tsx
+++ src/components/clipboard-control.tsx
@@ -1,25 +1,29 @@
-import { Alert } from "@blueprintjs/core";
+import { Button, Classes, Dialog } from "@blueprintjs/core";
 import React from "react";
 import { useClipboardControl } from "../machines/actions";
 import { useTranslate } from "../machines/state";
 import { Grid } from "./ui/grid";
-import { GridItem } from "./ui/grid-item";
 
 export const ClipboardControl: React.FC = () => {
   const translate = useTranslate();
   const [currentlyProviding, provideNext, destroyClipboard] = useClipboardControl();
 
   return (
-    <Alert isOpen={currentlyProviding !== null} onCancel={destroyClipboard} onConfirm={provideNext}
-      cancelButtonText={translate.clipboard.cancelButton} confirmButtonText={translate.clipboard.nextButton}
-      icon="paperclip" canEscapeKeyCancel canOutsideClickCancel>
-      <Grid columnSpec="1fr 1fr" gap={10} width={[100, '%']}>
-        <GridItem colSpan={2}>{translate.clipboard.controlTitle}</GridItem>
-        <div>{translate.clipboard.secretName}</div>
-        <div>{currentlyProviding?.secret_name}</div>
-        <div>{translate.clipboard.property}</div>
-        <div>{currentlyProviding && translate.secret.property(currentlyProviding.property)}</div>
-      </Grid>
-    </Alert>
+    <Dialog isOpen={currentlyProviding !== null} onClose={destroyClipboard} title={translate.clipboard.controlTitle} icon="paperclip" usePortal canEscapeKeyClose canOutsideClickClose shouldReturnFocusOnClose>
+      <div className={Classes.DIALOG_BODY}>
+        <Grid columnSpec="1fr 1fr" gap={10} width={[100, '%']}>
+          <div>{translate.clipboard.secretName}</div>
+          <div>{currentlyProviding?.secret_name}</div>
+          <div>{translate.clipboard.property}</div>
+          <div>{currentlyProviding && translate.secret.property(currentlyProviding.property)}</div>
+        </Grid>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={destroyClipboard}>{translate.clipboard.cancelButton}</Button>
+          <Button intent="primary" onClick={provideNext}>{translate.clipboard.nextButton}</Button>
+        </div>
+      </div>
+    </Dialog>
   )
 }
--- src/components/zoom-display.tsx
+++ src/components/zoom-display.tsx
@@ -1,13 +1,13 @@
-import { Alert } from "@blueprintjs/core";
+import { Dialog } from "@blueprintjs/core";
 import React from "react";
 import { useRecoilState, useRecoilValueLoadable } from "recoil";
 import { selectedSecretVersionState, zoomSecretPropertyState } from "../machines/state";
 import { Flex } from "./ui/flex";
 import { css } from "@emotion/css";
+import { FlexItem } from "./ui/flex-item";
 
 const charOuter = css({
   position: "relative",
-  padding: "2px",
 });
 
 const charMarker = css({
@@ -31,18 +31,21 @@ export const ZoomDisplay: React.FC = () => {
   const currentSecretVersion = useRecoilValueLoadable(selectedSecretVersionState);
   const [zoomSecretProperty, setZoomSecretProperty] = useRecoilState(zoomSecretPropertyState);
 
+  const title = currentSecretVersion.state == "hasValue" && currentSecretVersion.contents !== undefined && zoomSecretProperty !== null ? `${currentSecretVersion.contents.name}: ${zoomSecretProperty}` : "";
   const chars = currentSecretVersion.state == "hasValue" && currentSecretVersion.contents !== undefined && zoomSecretProperty !== null ? Array.from(currentSecretVersion.contents.properties[zoomSecretProperty]) : [];
 
   return (
-    <Alert isOpen={chars.length > 0} onClose={() => { setZoomSecretProperty(null) }} canEscapeKeyCancel canOutsideClickCancel>
-      <Flex flexDirection="row" gap={5} flexWrap="wrap">
-        {chars.map((ch, idx) => (
-          <div key={idx} className={charOuter}>
-            <div className={charInner}>{ch}</div>
-            <div className={charMarker} />
-          </div>
-        ))}
-      </Flex>
-    </Alert>
+    <Dialog isOpen={chars.length > 0} title={title} onClose={() => { setZoomSecretProperty(null) }} usePortal canEscapeKeyClose canOutsideClickClose shouldReturnFocusOnClose>
+      <FlexItem alignSelf="center" padding={5}>
+        <Flex flexDirection="row" gap={5} flexWrap="wrap">
+          {chars.map((ch, idx) => (
+            <div key={idx} className={charOuter}>
+              <div className={charInner}>{ch}</div>
+              <div className={charMarker} />
+            </div>
+          ))}
+        </Flex>
+      </FlexItem>
+    </Dialog>
   );
 }
