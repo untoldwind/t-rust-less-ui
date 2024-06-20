dabd86fb903b004bcad191da6c446d971049daf1
Bodo Junglas <junglas@objectcode.de>
2024-06-20 19:55:08 +0200

    Simple backend state

--- src/backend/provider.tsx
+++ src/backend/provider.tsx
@@ -1,16 +1,96 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
+import {
+  Identity,
+  Result,
+  ServiceError,
+  StoreConfig,
+  commands,
+} from "./bindings";
 
 export interface BackendState {
+  stores?: StoreConfig[];
+  selectedStore?: string;
+  identities?: Identity[];
+  errors: ServiceError[];
   loading: boolean;
+  clearError: (idx: number) => void;
 }
 
 export const BackendContext = React.createContext<BackendState>({
   loading: false,
+  errors: [],
+  clearError: () => {},
 });
 
 export const BackendProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [stores, setStores] = useState<StoreConfig[] | undefined>(undefined);
+  const [selectedStore, setSelectedStore] = useState<string | undefined>(
+    undefined
+  );
+  const [identities, setIdentities] = useState<Identity[] | undefined>(
+    undefined
+  );
+  const tasks = useRef<Set<string>>(new Set());
+  const [errors, setErrors] = useState<ServiceError[]>([]);
+
+  function runTask<T>(
+    taskName: string,
+    taskFn: () => Promise<Result<T, ServiceError>>,
+    handleOk: (value: T) => void
+  ) {
+    if (tasks.current.has("identities")) return;
+    tasks.current.add(taskName);
+    taskFn()
+      .then((result) => {
+        tasks.current.delete(taskName);
+        if (result.status === "ok") {
+          handleOk(result.data);
+        } else {
+          setErrors([...errors, result.error]);
+        }
+      })
+      .catch((error) => {
+        tasks.current.delete(taskName);
+        setErrors([...errors, { IO: error.toString() }]);
+      });
+  }
+
+  function clearError(idx: number): void {
+    setErrors([...errors.slice(0, idx), ...errors.slice(idx + 1)]);
+  }
+
+  useEffect(() => {
+    runTask(
+      "stores",
+      () => commands.serviceListStores(),
+      (stores) => {
+        setStores(stores);
+        if (selectedStore === undefined && stores.length > 0) {
+          setSelectedStore(stores[0].name);
+        }
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (selectedStore === undefined) return;
+    runTask(
+      "identities",
+      () => commands.storeIdentities(selectedStore),
+      setIdentities
+    );
+  }, [selectedStore]);
+
   return (
-    <BackendContext.Provider value={{ loading: false }}>
+    <BackendContext.Provider
+      value={{
+        loading: tasks.current.size === 0,
+        stores,
+        identities,
+        errors,
+        clearError,
+      }}
+    >
       {children}
     </BackendContext.Provider>
   );
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Unlock } from "./unlock/unlock";
+import { BackendContext } from "../backend/provider";
 
 export interface NavigationState {
   page: "Unlock";
@@ -11,6 +12,9 @@ export const NavigationContext = React.createContext<NavigationState>({
 
 export const Navigation: React.FC = () => {
   const [page, _] = React.useState<NavigationState["page"]>("Unlock");
+  const backend = React.useContext(BackendContext);
+
+  console.log(backend);
 
   function pageComponent() {
     switch (page) {
